bb10417d7263cd00e6bcd41d322bb5915d1b24de
Omarabdul3ziz <omarabdul3ziz@gmail.com>
2021-11-17 13:28:34 +0200

    save branch

--- src/elements/funkwhale/Funkwhale.wc.svelte
+++ src/elements/funkwhale/Funkwhale.wc.svelte
@@ -1,7 +1,287 @@
+<!-- // // [1] Tabs
+ //     - single tab (specs)
+
+ // // [2] content
+ //     - SolutonName : text
+ //     - Package  : radio (s,g,d) >> take input do nothing
+ //     1cpu+ 4ram =8disk
+
+ // // [3] deploing with dynamic capacity with prefix_gateway
+ // // funkwhale_audio.balbla
+ //     - call the grid3_client_ts to deploy
+
+ // // [4] return the info
+ //     - list of (solution name, machine ip, domain)
+ //     - buton to shoe more -->
 <svelte:options tag={null} />
 
-<script>
-  let msg = "Hello, Funkwhale";
+<script lang="ts">
+  import VM, { Disk, Env } from "../../types/vm";
+  import type { IFormField } from "../../types";
+  const { events } = window.configs?.grid3_client ?? {};
+  import deployVM from "../../utils/deployVM";
+
+  const data = new VM();
+
+  const tabs = [
+    { label: "Base" },
+    { label: "Envs" },
+    // { label: "Packages" },
+    { label: "Configs" },
+  ];
+
+  let active: string = "Base";
+  let loading = false;
+  let success = false;
+  let failed = false;
+
+  // prettier-ignore
+  const baseFields: IFormField[] = [
+    { label: "Name", symbol: 'name', placeholder: 'Your VM name.'},
+    { label: "Public IP", symbol: "publicIp", placeholder: "", type: 'checkbox' },
+    { label: "Planetary", symbol: "planetary", placeholder: "", type: 'checkbox' },
+    ];
+
+  // prettier-ignore
+  const envFields: IFormField[] = [
+    { label: 'Key', symbol: 'key', placeholder: "Your Env Key."},
+    { label: 'Value', symbol: 'value', placeholder: "Your Env Value."},
+  ];
+
+  // prettier-ignore
+  const configFields: IFormField[] = [
+    { label: "Mnemonics", symbol: "mnemonics", placeholder: "Your Mnemonics." },
+    { label: "Store Secret", symbol: "storeSecret", placeholder: "Your Store Secret." },
+  ];
+
+  let message: string;
+  function onDeployVM() {
+    loading = true;
+    success = false;
+    failed = false;
+
+    function onLogInfo(msg: string) {
+      if (typeof msg === "string") {
+        message = msg;
+      }
+    }
+    events.addListener("logs", onLogInfo);
+
+    deployVM(data)
+      .then(() => (success = true))
+      .catch((err) => {
+        failed = true;
+        console.log("Error", err);
+      })
+      .finally(() => {
+        loading = false;
+        events.removeListener("logs", onLogInfo);
+      });
+  }
 </script>
 
-<h1>{msg}</h1>
+<div style="padding: 15px;">
+  <form on:submit|preventDefault={onDeployVM} class="box">
+    <h4 class="is-size-4">Deploy a Funkwhale Instance</h4>
+    <hr />
+
+    {#if loading}
+      <div class="notification is-info">
+        {#if message}
+          &gt; {message}.
+        {:else}
+          &gt; Loading...
+        {/if}
+      </div>
+    {:else if success}
+      <div class="notification is-success">&gt; Successfully deployed VM.</div>
+    {:else if failed}
+      <div class="notification is-danger">&gt; Failed to deploy VM.</div>
+    {:else}
+      <div
+        class="select mb-4"
+        style="display: flex; justify-content: flex-end;"
+      >
+        <select bind:value={data.configs.networkEnv}>
+          <option value="test">Testnet</option>
+          <option value="dev">Devnet</option>
+        </select>
+      </div>
+      <div class="tabs is-centered">
+        <ul>
+          {#each tabs as tab (tab.label)}
+            <li class={active === tab.label ? "is-active" : ""}>
+              <a href="#!" on:click|preventDefault={() => (active = tab.label)}>
+                <span>{tab.label}</span>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+
+      {#if active === "Base"}
+        <!-- Show Base Info -->
+        {#each baseFields as field (field.symbol)}
+          <div class="field">
+            <p class="label">{field.label}</p>
+            <div class="control">
+              {#if field.type === "number"}
+                <input
+                  class="input"
+                  type="number"
+                  placeholder={field.placeholder}
+                  bind:value={data[field.symbol]}
+                />
+              {:else if field.type === "checkbox"}
+                <label class="checkbox">
+                  <input
+                    type="checkbox"
+                    checked={data[field.symbol]}
+                    on:change={() => (data[field.symbol] = !data[field.symbol])}
+                  />
+                  {field.label}
+                </label>
+              {:else}
+                <input
+                  class="input"
+                  type="text"
+                  placeholder={field.placeholder}
+                  bind:value={data[field.symbol]}
+                />
+              {/if}
+            </div>
+          </div>
+        {/each}
+
+        <!-- Network info -->
+        <!-- {#each networkFields as field (field.symbol)}
+          <div class="field">
+            <p class="label">{field.label}</p>
+            <div class="control">
+              <input
+                class="input"
+                type="text"
+                placeholder={field.placeholder}
+                bind:value={data.network[field.symbol]}
+              />
+            </div>
+          </div>
+        {/each} -->
+      {/if}
+
+      {#if active === "Envs"}
+        <div class="actions" style="margin-bottom: 20px;">
+          <button
+            type="button"
+            class="button is-primary is-light"
+            on:click={() => (data.envs = [...data.envs, new Env()])}
+          >
+            <span>+ ADD Env</span>
+          </button>
+        </div>
+        <div class="vm-container">
+          {#each data.envs as env, index (env.id)}
+            <div class="box">
+              <div class="vm-header">
+                <p class="is-size-5 has-text-weight-bold">{env.key}</p>
+                <button
+                  type="button"
+                  class="button is-danger is-outlined"
+                  on:click={() =>
+                    (data.envs = data.envs.filter((_, i) => index !== i))}
+                >
+                  <span>Delete</span>
+                </button>
+              </div>
+              {#each envFields as field (field.symbol)}
+                <div class="field">
+                  <p class="label">{field.label}</p>
+                  <div class="control">
+                    <input
+                      class="input"
+                      type="text"
+                      placeholder={field.placeholder}
+                      bind:value={env[field.symbol]}
+                    />
+                  </div>
+                </div>
+              {/each}
+            </div>
+          {/each}
+        </div>
+      {/if}
+
+      {#if active === "Configs"}
+        {#each configFields as field (field.symbol)}
+          <div class="field">
+            <p class="label">{field.label}</p>
+            <div class="control">
+              {#if field.type === "number"}
+                <input
+                  class="input"
+                  type="number"
+                  placeholder={field.placeholder}
+                  bind:value={data.configs[field.symbol]}
+                />
+              {:else}
+                <input
+                  class="input"
+                  type="text"
+                  placeholder={field.placeholder}
+                  bind:value={data.configs[field.symbol]}
+                />
+              {/if}
+            </div>
+          </div>
+        {/each}
+      {/if}
+    {/if}
+
+    <div class="actions">
+      <button
+        class={"button is-primary " + (loading ? "is-loading" : "")}
+        type="submit"
+        disabled={(loading || !data.valid) && !(success || failed)}
+        on:click={(e) => {
+          if (success || failed) {
+            e.preventDefault();
+            success = false;
+            failed = false;
+            loading = false;
+          }
+        }}
+      >
+        {#if success || failed}
+          Back
+        {:else}
+          Deploy
+        {/if}
+      </button>
+    </div>
+  </form>
+</div>
+
+<style lang="scss" scoped>
+  @import url("https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css");
+
+  .actions {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+
+  .vm-container {
+    overflow-x: hidden;
+    overflow-y: auto;
+    max-height: 70vh;
+    will-change: transform;
+    padding-bottom: 5rem;
+    margin-bottom: 20px;
+  }
+
+  .vm-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
